1b72805ed59783b768b8666f12b0c1eb13c6d542
Žikica Drača <zikica.draca@gmail.com>
2024-11-23 19:12:12 +0100

    buttonSec corection

--- src/components/button-2/buttonSec.jsx
+++ src/components/button-2/buttonSec.jsx
@@ -1,10 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './buttonSec.css';
 
 const ButtonSec = ({ children, bg, ani, url }) => {
+  const location = useLocation();
+  const linkUrl = url || location.pathname;
   return (
-    <Link to={`${url}`} className={`button-sec ${bg} ${ani}`}>
+    <Link to={linkUrl} className={`button-sec ${bg} ${ani}`}>
       {children}
     </Link>
   );
@@ -14,7 +16,7 @@ ButtonSec.propTypes = {
   children: PropTypes.node,
   bg: PropTypes.string,
   ani: PropTypes.string,
-  url: PropTypes.number,
+  url: PropTypes.string,
 };
 
 export default ButtonSec;
